9a9795b537b61943d3e5673292e6fd3087050413
Thomas Luquet <thomas@luquet.net>
2017-03-30 16:25:08 +0200

    update doc

--- playerctl.el
+++ playerctl.el
@@ -10,9 +10,21 @@
   (start-process-shell-command "playerctl.el" "play" "playerctl" "play-pause")
   (message "Play or pause the music"))
 
+(defun next-song()
+  "Change the song by the next one "
+  (interactive)
+  (start-process-shell-command "playerctl.el" "play" "playerctl" "next")
+  (message "Next song"))
+
+(defun previous-song()
+  "Change the song by the last one "
+  (interactive)
+  (start-process-shell-command "playerctl.el" "play" "playerctl" "previous")
+  (message "previous song"))
+
 
 (defun stop-music()
-  "Stop the music"
+  "Stop music"
   (interactive)
   (start-process-shell-command "playerctl.el" "stop" "playerctl" "stop")
-  (message "Stop the music"))
+  (message "Stop music"))
