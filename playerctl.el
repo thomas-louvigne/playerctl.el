e6e4576783e1901d8d4c396231e18953f5252685
Thomas Luquet <thomas@luquet.net>
2017-03-31 13:59:02 +0200

    activate flycheck-mode , add a commentary and rename process

--- playerctl.el
+++ playerctl.el
@@ -1,4 +1,9 @@
-
+;;; package --- Summary
+;;; Simple package to play music from emacs with playerctl
+;;; Commentary:
+;;; You need to have playerctl installed
+;;; More info on https://github.com/thomasluquet/playerctl.el
+;;; Code:
 (defgroup playerctl.el nil
   "Control your music from emacs with playerctl."
   :group 'multimedia)
@@ -7,19 +12,19 @@
 (defun play-pause-music()
   "Play or pause the music"
   (interactive)
-  (start-process-shell-command "playerctl.el" "play" "playerctl" "play-pause")
+  (start-process-shell-command "playerctl.el" "playOrPause" "playerctl" "play-pause")
   (message "Play or pause the music"))
 
 (defun next-song()
   "Change the song by the next one "
   (interactive)
-  (start-process-shell-command "playerctl.el" "play" "playerctl" "next")
+  (start-process-shell-command "playerctl.el" "next" "playerctl" "next")
   (message "Next song"))
 
 (defun previous-song()
   "Change the song by the last one "
   (interactive)
-  (start-process-shell-command "playerctl.el" "play" "playerctl" "previous")
+  (start-process-shell-command "playerctl.el" "previous" "playerctl" "previous")
   (message "previous song"))
 
 
@@ -28,3 +33,5 @@
   (interactive)
   (start-process-shell-command "playerctl.el" "stop" "playerctl" "stop")
   (message "Stop music"))
+
+;;; playerctl.el ends here
