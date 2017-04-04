3d0bba75b2815d027973a8977cbb0cd674667dba
Thomas Luquet <thomas@luquet.net>
2017-04-04 16:19:31 +0200

    add gpl lines

--- playerctl.el
+++ playerctl.el
@@ -2,18 +2,31 @@
 ;;
 ;; Author: Thomas Luquet <thomas@luquet.net>
 ;; Keywords: multimedia, playerctl, music
-;; Version: 0.1
+;; URL: https://github.com/thomasluquet/playerctl.el
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;
 ;;; Commentary:
-;; Play with Spotify, vlc, audacious, bmp, xmms2, and others music from Emacs with playerctl
+;; Play music with Spotify, vlc, audacious, bmp, xmms2, and others music player from Emacs with playerctl
 ;;
-;; /!\ You need to have playerctl installed : https://github.com/acrisci/playerctl
+;;; Dependencies:
+;; You need to have playerctl installed : <https://github.com/acrisci/playerctl>
 ;;
-;; Use:
+;;; Customization:
 ;; (require 'playerctl)
 ;; (define-key global-map (kbd "C-c C-SPC") 'playerctl-play-pause-song)
 ;; (define-key global-map (kbd "C-c C-n") 'playerctl-next-song)
-;; More info on https://github.com/thomasluquet/playerctl.el
 ;;
 ;;; Code:
 
