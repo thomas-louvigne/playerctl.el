2a3e1a7ab36329f21b284bcc951a164e78767cc6
Thomas Luquet <thomas@luquet.net>
2017-04-04 15:55:16 +0200

    apply package linter and add playerctl- prefix for function

--- playerctl.el
+++ playerctl.el
@@ -1,17 +1,15 @@
-;;; Playerctl.el --- All your music from Emacs
-;; Simple package to play with your music from your music player (Spotify, vlc, audacious, bmp, xmms2, and others) from Emacs with playerctl
+;;; Playerctl.el --- Use your music player (Spotify...) with playerctl
 ;;
 ;; Author: Thomas Luquet <thomas@luquet.net>
-;; Keywords: playerctl, music
+;; Keywords: multimedia, playerctl, music
 ;; Version: 0.1
 ;;
 ;;; Commentary:
-;; Simple package to play with your music from your music player (Spotify, vlc, audacious, bmp, xmms2, and others) from Emacs with playerctl
+;; Play with Spotify, vlc, audacious, bmp, xmms2, and others music from Emacs with playerctl
 ;;
 ;; /!\ You need to have playerctl installed : https://github.com/acrisci/playerctl
 ;;
 ;; Use:
-;;
 ;; (require 'playerctl)
 ;; (define-key global-map (kbd "C-c C-SPC") 'play-pause-song)
 ;; (define-key global-map (kbd "C-c C-n") 'next-song)
@@ -23,26 +21,26 @@
   "Control your music from emacs with playerctl."
   :group 'multimedia)
 
-(defun play-pause-song()
+(defun playerctl-play-pause-song()
   "Play or pause the song"
   (interactive)
   (start-process-shell-command "playerctl.el" "playOrPause" "playerctl" "play-pause")
   (message "Play or pause the current song"))
 
-(defun next-song()
+(defun playerctl-next-song()
   "Change the song by the next one"
   (interactive)
   (start-process-shell-command "playerctl.el" "next" "playerctl" "next")
   (message "Next song"))
 
-(defun previous-song()
+(defun playerctl-previous-song()
   "Change the song by the last one"
   (interactive)
   (start-process-shell-command "playerctl.el" "previous" "playerctl" "previous")
   (message "Previous song"))
 
-(defun stop-music()
-  "Stop music"
+(defun playerctl-stop-song()
+  "Stop song"
   (interactive)
   (start-process-shell-command "playerctl.el" "stop" "playerctl" "stop")
   (message "Stop music"))
