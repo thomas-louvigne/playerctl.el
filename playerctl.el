c539e80a2ecaa99a36aec438035da3437df0c6c1
Thomas Luquet <thomas@luquet.net>
2017-04-04 14:23:02 +0200

    BSR + rename func

--- playerctl.el
+++ playerctl.el
@@ -8,7 +8,13 @@
 ;;; Commentary:
 ;; Simple package to play with your music from your music player (Spotify, vlc, audacious, bmp, xmms2, and others) from Emacs with playerctl
 ;;
-;; You need to have playerctl installed
+;; /!\ You need to have playerctl installed : https://github.com/acrisci/playerctl
+;;
+;; Use:
+;;
+;; (require 'playerctl)
+;; (define-key global-map (kbd "C-c C-SPC") 'play-pause-song)
+;; (define-key global-map (kbd "C-c C-n") 'next-song)
 ;; More info on https://github.com/thomasluquet/playerctl.el
 ;;
 ;;; Code:
@@ -17,25 +23,23 @@
   "Control your music from emacs with playerctl."
   :group 'multimedia)
 
-
-(defun play-pause-music()
-  "Play or pause the music"
+(defun play-pause-song()
+  "Play or pause the song"
   (interactive)
   (start-process-shell-command "playerctl.el" "playOrPause" "playerctl" "play-pause")
-  (message "Play or pause the music"))
+  (message "Play or pause the current song"))
 
 (defun next-song()
-  "Change the song by the next one "
+  "Change the song by the next one"
   (interactive)
   (start-process-shell-command "playerctl.el" "next" "playerctl" "next")
   (message "Next song"))
 
 (defun previous-song()
-  "Change the song by the last one "
+  "Change the song by the last one"
   (interactive)
   (start-process-shell-command "playerctl.el" "previous" "playerctl" "previous")
-  (message "previous song"))
-
+  (message "Previous song"))
 
 (defun stop-music()
   "Stop music"
